66d1b490ded55cd701362c785859e668a209592f
Baylee Feore <baylee.feore@gmail.com>
2014-08-01 16:18:56 -0700

    Update views.py

--- fx_app/views.py
+++ fx_app/views.py
@@ -11,6 +11,7 @@ def home(request):
 def tweets(request):
 
     country = request.POST['country']
+    # Good job mvoing your key and secret into settings
     auth = OAuthHandler(settings.CKEY, settings.CSECRET)
     auth.set_access_token(settings.ATOKEN,settings.ASECRET)
     api = tweepy.API(auth)
@@ -20,12 +21,14 @@ def tweets(request):
     tweet_list = []
     search_results = api.search(q=country, count=10000, lang="en")
     for tweet in search_results:
+        # print statements should only be for debugging, and should be removed
         print tweet.text
         tweet_list.append(unicode(tweet.text))
     analysis_results = analysis(tweet_list)
     data = {'content': tweet_list, 'analysis_results': analysis_results}
     return render(request, "tweets.html", data)
 
+# This should NOT live in your views file. Only views go here. This code should live elsewhere.
 def analysis(tweet_list):
 
     #links to NLP sentiment analysis at http://text-processing.com/demo/sentiment/
@@ -33,12 +36,16 @@ def analysis(tweet_list):
     payload = {'text':tweet_list}
     response = requests.post(url, data=payload)
     return_string = response.content
+    # Instead of taking JSON results in as a string and then using a regex, you could have made this request w/
+    # AJAX. Your JS file would have received the JSON response, and it would be able to parse through it 'normally'
+    # e.g., as a dictionary / JS object
     numbers = re.findall('\d+(\.\d{1,5})', return_string)
 
     neg_int = float(numbers[0])
     # neutral_int = float(numbers[1])
     pos_int = float(numbers[2])
 
+    # What is this if-statement for? Doesn't the API tell you Positive or Negative?
     if pos_int > neg_int:
         label = "POSITIVE"
     else:
