c984755843e2b9359bbaabcd18c4010dbdcc23b8
Baylee Feore <baylee.feore@gmail.com>
2014-08-01 16:22:09 -0700

    Update models.py

--- fx_app/models.py
+++ fx_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+
+# What is this model for?
 class Article(models.Model):
     date = models.CharField(max_length=300)
     snippet = models.TextField()
