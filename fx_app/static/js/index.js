a2a06606032f9b0396d5c94c3d07943c4746a916
Greg Levin <ljz00us@gmail.com>
2014-08-03 14:47:55 -0700

    latest version

--- fx_app/static/js/index.js
+++ fx_app/static/js/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by gregorylevin on 7/26/14.
  */
+
+$(document).ready(function () {
+
+    $('#submit').submit(function(){
+       if($('#query').val() == ''){
+           alert("Please enter text.");
+           return true;
+       }
+    });
+});
