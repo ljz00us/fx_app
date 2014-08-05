cfb756b4f5303ff3b66b051bccd9de964f179679
Greg Levin <ljz00us@gmail.com>
2014-08-04 19:28:40 -0700

    Push to heroku

--- fx/urls.py
+++ fx/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
     # Examples:
     # url(r'^$', 'fx.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
--- fx/wsgi.py
+++ fx/wsgi.py
@@ -13,8 +13,8 @@ from dj_static import Cling
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fx.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
 
 application = Cling(get_wsgi_application())
